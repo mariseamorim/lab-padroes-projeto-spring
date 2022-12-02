6b243d08177bb8d6963f0cc66403edff0f6cb9b3
Marise Amorim <marisemfs@gmail.com>
2022-12-01 21:26:55 -0300

    Iniciando implementação do desafio

---
+++ src/main/java/one/digitalinnovation/gof/repository/ClienteRepository.java
@@ -0,0 +1,2 @@
+package one.digitalinnovation.gof.repository;public class ClienteRepository {
+}
---
+++ src/main/java/one/digitalinnovation/gof/repository/EnderecoRepository.java
@@ -0,0 +1,2 @@
+package one.digitalinnovation.gof.repository;public interface EnderecoRepository {
+}
